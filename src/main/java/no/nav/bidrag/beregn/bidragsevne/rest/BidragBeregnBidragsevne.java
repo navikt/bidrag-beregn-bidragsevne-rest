b6975ead959da99804c15c9185ead1098ceb5b65
LarsOttoHaugen <lars.otto.haugen@nav.no>
2020-04-29 12:30:56 +0200

    Initiell versjon

---
+++ src/main/java/no/nav/bidrag/beregn/bidragsevne/rest/BidragBeregnBidragsevne.java
@@ -0,0 +1,13 @@
+package no.nav.bidrag.beregn.bidragsevne.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidragBeregnBidragsevne {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidragBeregnBidragsevne.class, args);
+	}
+
+}
---
+++ src/test/java/no/nav/bidrag/beregn/bidragsevne/rest/BidragBeregnBidragsevneTests.java
@@ -0,0 +1,13 @@
+package no.nav.bidrag.beregn.bidragsevne.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidragBeregnBidragsevneTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
