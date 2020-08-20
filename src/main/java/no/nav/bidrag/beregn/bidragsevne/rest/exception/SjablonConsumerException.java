d68e8207c2f5cb287ee47e8716b44339aec1a9d7
LarsOttoHaugen <lars.otto.haugen@nav.no>
2020-08-20 13:17:40 +0200

    Forbedret feilhåndtering ved kall til andre rest-tjenester (sjablon)

--- src/main/java/no/nav/bidrag/beregn/bidragsevne/rest/exception/SjablonConsumerException.java
+++ src/main/java/no/nav/bidrag/beregn/bidragsevne/rest/exception/SjablonConsumerException.java
@@ -1,8 +1,18 @@
 package no.nav.bidrag.beregn.bidragsevne.rest.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestClientResponseException;
+
 public class SjablonConsumerException extends RuntimeException {
 
-  public SjablonConsumerException(String melding) {
-    super(melding);
+  private HttpStatus statusCode;
+
+  public HttpStatus getStatusCode() {
+    return statusCode;
+  }
+
+  public SjablonConsumerException(RestClientResponseException exception) {
+    super(exception);
+    this.statusCode = HttpStatus.valueOf(exception.getStatusText());
   }
 }
